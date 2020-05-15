a13a0cd203ee0e425c51280ea9dd24a0ccd78624
y2k-shubham <y2k.shubhamgupta@gmail.com>
2020-05-15 23:54:07 +0530

    [dev] (arrays_sorting_searching/kth_smallestlargest_element_unsorted_array) Add 'QuickSelect' function

--- arrays_sorting_searching/kth_smallestlargest_element_unsorted_array/kth_smallestlargest_element_unsorted_array.go
+++ arrays_sorting_searching/kth_smallestlargest_element_unsorted_array/kth_smallestlargest_element_unsorted_array.go
@@ -3,6 +3,12 @@
 
 package kth_smallestlargest_element_unsorted_array
 
+import (
+	"errors"
+	"fmt"
+	"math"
+)
+
 func Swap(arr []int, i int, j int) {
 	tmp := arr[i]
 	arr[i] = arr[j]
@@ -25,4 +31,23 @@ func Partition(arr []int, lo int, hi int) int {
 	Swap(arr, j, pivotInd)
 
 	return j
+}
+
+func QuickSelect(k int, arr []int, lo int, hi int) (int, error) {
+	if lo > hi {
+		return math.MinInt32, errors.New(fmt.Sprintf("lo=%d can't exceed hi=%d", lo, hi))
+	} else if k - 1 < lo {
+		return math.MinInt32, errors.New(fmt.Sprintf("k-1=%d can't be less than lo=%d", k-1, lo))
+	} else if k - 1 > hi {
+		return math.MinInt32, errors.New(fmt.Sprintf("k-1=%d can't exceed hi=%d", k-1, hi))
+	} else {
+		pivotInd := Partition(arr, lo, hi)
+		if k < pivotInd+1 {
+			return QuickSelect(k, arr, lo, pivotInd-1)
+		} else if pivotInd+1 == k {
+			return arr[pivotInd], nil
+		} else {
+			return QuickSelect(k, arr, pivotInd+1, hi)
+		}
+	}
 }
